b982ffea8615d216a204ff97b17a2e94528876c3
baxtiyor <baxtiyorproweb4437>
2022-07-20 18:28:00 +0500

    javascript

---
+++ script.js
@@ -0,0 +1,26 @@
+// ism
+var ism = prompt("Ismingiz nima?")
+console.log("Sizning ismingiz " + ism)
+// yosh
+var yosh = prompt("Yoshingiz nechida?")
+console.log("Sizning ismingiz " + yosh + " da")
+// misol 1 
+var misol1 = prompt("Misolni yeching 20 - 9 =")
+console.log("1-misolning javobi 11;" + " Sizning javob " + misol1)
+// misol2
+var misol2 = prompt("Misolni yeching 100 - 10 =")
+console.log("2-misolning javobi 90;" + " Sizning javob " + misol2)
+// misol3
+var misol3 = prompt("Misolni yeching 11 * 9 =")
+console.log("3-misolning javobi 99;" + " Sizning javob " + misol3)
+// misol4
+var misol4 = prompt("Misolni yechging 22 : 11 =")
+console.log("4-misolning javobi 2;" + " Sizning javob " + misol4)
+// misol5
+var misol5 = prompt("Misolni yechging 0 * 788  =")
+console.log("5-misolning javobi 0;" + " Sizning javob " + misol5)
+// finish
+alert("Javoblar console bo'limida")
+
+
+
